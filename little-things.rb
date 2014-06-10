24561c19d7a2f2dcb022536243e24f5a105b1ad4
Kristoffer Grönlund <krig@koru.se>
2014-06-10 23:43:26 +0200

    I sort of like the stabby lambdas

--- little-things.rb
+++ little-things.rb
@@ -1,43 +1,42 @@
-TICK = {
-  "normal" => lambda do |days, quality|
-    days -= 1
-    return days, quality if quality == 0
-    return days, quality - 2 if days < 1
-    return days, quality - 1
-  end,
-  "Aged Brie" => lambda do |days, quality|
-    days -= 1
-    return days, quality if quality >= 50
-    return days, quality - 2 if days < 1
-    return days, quality - 1
-  end,
-  "Sulfuras, Hand of Ragnaros" => lambda do |days, quality|
-    return days, quality
-  end,
-  "Backstage passes to a TAFKAL80ETC concert" => lambda do |days, quality|
-    days -= 1
-    return days, quality if quality >= 50
-    return days, 0 if days < 0
-    return days, quality + 3 if days < 5
-    return days, quality + 2 if days < 10
-    return days, quality + 1
-  end,
-  "Conjured Mana Cake" => lambda do |days, quality|
-    days -= 1
-    return days, quality if quality == 0
-    return days, quality - 4 if days <= 0
-    return days, quality - 2
-  end,
-}
-
 class GildedRose
   attr_accessor :quality, :days_remaining
+
   def initialize(name, quality, days_remaining)
-    @tick = TICK[name]
-    @quality = quality
-    @days_remaining = days_remaining
+    tick = {
+      "normal" => ->(days, quality) {
+        days -= 1
+        return days, quality if quality == 0
+        return days, quality - 2 if days < 1
+        return days, quality - 1
+      },
+      "Aged Brie" => ->(days, quality) {
+        days -= 1
+        return days, quality if quality >= 50
+        return days, quality - 2 if days < 1
+        return days, quality - 1
+      },
+      "Sulfuras, Hand of Ragnaros" => ->(days, quality) {
+        return days, quality
+      },
+      "Backstage passes to a TAFKAL80ETC concert" => ->(days, quality) {
+        days -= 1
+        return days, quality if quality >= 50
+        return days, 0 if days < 0
+        return days, quality + 3 if days < 5
+        return days, quality + 2 if days < 10
+        return days, quality + 1
+      },
+      "Conjured Mana Cake" => ->(days, quality) {
+        days -= 1
+        return days, quality if quality == 0
+        return days, quality - 4 if days <= 0
+        return days, quality - 2
+      },
+    }
+    @days_remaining, @quality, @tick = days_remaining, quality, tick[name]
   end
+
   def tick
-    @days_remaining, @quality = @tick.call @days_remaining, @quality
+    @days_remaining, @quality = @tick.(@days_remaining, @quality)
   end
 end
